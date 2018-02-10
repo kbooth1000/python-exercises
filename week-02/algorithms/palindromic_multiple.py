4ad04eba9fa13a1327434620478cc1a7d765b586
Kyle Booth <kbooth1000@gmail.com>
2018-02-10 17:52:52 -0500

    Change variable names for readability

--- week-02/algorithms/palindromic_multiple.py
+++ week-02/algorithms/palindromic_multiple.py
@@ -1,13 +1,17 @@
-# multiply 999 by 999 -=1
-# eval if list(str(m)) = reverse(list(str(m)))
+###  Find the largest palindromic number 
+###  made from the product of two 3-digit numbers.
+
+
+# multiply 999 by 999 -=1...0, then 998 by 998 -=1...0, etc.
+# eval if list(str(product)) = reverse(list(str(product)))
 
 finish = False
-for m in range(999, 99, -1):
-    for n in range (999, 99, -1):
-        num_str = str(m * n)
-        print '%d * %d = %d' % (m, n, m*n)
+for factor_1 in range(999, 99, -1):
+    for factor_2 in range (999, 99, -1):
+        num_str = str(factor_1 * factor_2)
+        print '%d * %d = %d' % (factor_1, factor_2, factor_1 * factor_2)
         if num_str == num_str[ : : -1]:
-            print '*****  %d * %d = %d' % (m, n, m*n)
+            print '*****  %d * %d = %d' % (factor_1, factor_2, factor_1 * factor_2)
             finish = True
             break
     if finish:
